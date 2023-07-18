67cd484e9e4fa1d9fbb54d532e4434698e4e7673
Kubabuba14 <jakubwit@hotmail.co.uk>
2023-07-18 18:47:37 +0200

    setting up

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='wizzard' src= ''/>
+        <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@hotmail.com</p>
+        </div>
+        </div>
+    );
+}
+
+export default Card;
